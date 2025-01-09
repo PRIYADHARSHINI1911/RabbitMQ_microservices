67b453eb024ead8524747bb6463f603da6a1db55
Priyadharshini <priyakrish816@gmail.com>
2025-01-09 17:12:15 +0530

    Uploaded files

---
+++ spring-rabbitmq-producer/src/main/java/com/project/spring_rabbitmq_producer/CustomMessage.java
@@ -0,0 +1,41 @@
+package com.project.spring_rabbitmq_producer;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CustomMessage {
+
+    private String messageId;
+    private String message;
+    private Date messageDate;
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getMessageDate() {
+        return messageDate;
+    }
+
+    public void setMessageDate(Date messageDate) {
+        this.messageDate = messageDate;
+    }
+}
